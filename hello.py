b8a077eb82b01ed021d0da979d8b587b6be63da5
abdul malik khan <am6690707@gmail.com>
2026-07-23 22:52:20 +0530

    first day

---
+++ hello.py
@@ -0,0 +1,93 @@
+#print ("Hello World!")
+# name  = "Abdul "
+# Name  = "Malik "
+# Name  = "Khan "
+
+# print(name)
+# print(Name)
+# print(Name)
+
+
+# age =14
+# if age<18:
+#     print("Not Adult")
+# print(age)
+
+
+#line continuation
+# total = 1+2+3+2+7+\
+# 4+5+6
+# print (total)
+
+# college_name="ABES Engineering\
+#  college"
+# print(college_name)
+
+
+
+
+#Multiple statements on a single line
+# x=5;y=10;z=x+y
+# print(z)
+
+#semantic in python
+# age =24
+# name ="hello"    galat hai check karna hai
+
+
+# variable =10
+# print(type(variable))
+
+# variable= "abdul"
+# print(type(variable))
+
+# variable = 34.4
+# print(type(variable))
+
+
+
+# a = 10+6j
+# print(a)
+# print(type(a))
+
+
+
+# x= True
+# print(x)    
+# print(type(x))
+
+
+
+# xy = None
+# print(xy)
+# print(type(xy))
+
+
+
+#collection
+#list
+# A =[1,None,2.4,"Hello",6+6j,True]
+# print(A)
+# print(type(A))
+
+
+#difference b/w list and touple
+#[touple is more secure comparasion to list]
+
+
+#tuple
+# B = (1,None,2.4,1,"Hello",6+6j,True)
+# print(B)
+# print(type(B))
+
+
+#set[index number ko access nahi karta hai]
+# C = {1,None,2.4,1,"Hello",6+6j,True}
+# print(C)
+# print(type(C))
+
+
+#Dictionary[curly braces use hota hai]
+# D = {1:"a", 2:"b",3:"c"}
+# print(D)
+# print(type(D))
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+a = 10+6j
+print(a)
+print(type(a))
