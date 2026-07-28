1aec48b7c84da4d1ecb7f4dcc864a8d0257302c3
abdul malik khan <am6690707@gmail.com>
2026-07-28 13:10:09 +0530

    variables

---
+++ variables.py
@@ -0,0 +1,55 @@
+#naming conc=vention
+
+
+#typeconversion/typeCasting
+#two types:
+#1) Implicit TypeCasting  2) Explicit TypeCasting 
+#means Indirect       and  direct
+
+#Implicit Type casting
+# a = 10   #int 
+# b = 3  #float
+# c = a/b  #float
+# c= int (c)
+# print(a,type(a))
+# print(b,type(b))
+# print(c, int(c),type(c))
+
+
+
+
+# age = 25
+# print (type(age))
+
+# #type coversion
+# age_str = str(age)
+# print(age_str)
+# print(type(age_str))
+
+
+# x = "2342.43"
+# print(x,type(x))
+
+# y = float(x)
+# print(y,type(y))
+
+
+#taking input from the user
+# age = int(input("what is the age?"))
+# print(age, type(age))
+
+
+#simple calculator
+
+# num1 = float(input("Enter First Number: "))
+# num2 = float(input("Enter Second Number: "))
+
+# sum = num1+ num2
+# difference = num1-num2
+# product = num1 * num2
+# qoutient = num1/num2
+
+# print("Sum: ", sum)
+# print("Difference: ", sum)
+# print("Product: ", sum)
+# print("Qoutient: ", sum)
